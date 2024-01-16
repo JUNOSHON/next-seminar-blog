f2710eb57c23bb19c2ccd0a8d0e9d6543f0da194
junoshon <sanoldfox@naver.com>
2024-01-17 00:00:58 +0900

    feat: posstData 추가

--- src/app/service/posts.ts
+++ src/app/service/posts.ts
@@ -10,6 +10,8 @@ export type Post = {
   featured: boolean;
 };
 
+export type PostData = Post & { content: string };
+
 export async function getAllPosts(): Promise<Post[]> {
   const filePath = path.join(process.cwd(), "public", "data", "posts.json");
 
@@ -25,4 +27,22 @@ export async function getFeaturedPosts(): Promise<Post[]> {
 //피쳐되지 않은 포스트
 export async function getNonFeaturedPosts(): Promise<Post[]> {
   return getAllPosts().then((posts) => posts.filter((post) => !post.featured));
+}
+
+export async function getPostData(fileName: string): Promise<PostData> {
+  const filePath = path.join(
+    process.cwd(),
+    "public",
+    "data",
+    "posts",
+    `${fileName}.md`
+  );
+  const metadata = await getAllPosts().then((posts) =>
+    posts.find((post) => post.path === fileName)
+  ); //TODO: 다시 이해해보기
+  if (!metadata)
+    throw new Error(`${fileName} 에 해당하는 포스트를 찾을 수 없음`);
+
+  const content = await readFile(filePath, "utf-8");
+  return { ...metadata, content };
 }
