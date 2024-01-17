7048d0ff151363b95282876bb76bca95222f3ad0
junoshon <sanoldfox@naver.com>
2024-01-17 20:59:34 +0900

    feat: 마크다운 뷰어 코드 Syntax highlighting

--- src/app/components/MarkdownViewer.tsx
+++ src/app/components/MarkdownViewer.tsx
@@ -1,9 +1,44 @@
+"use client";
+
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import Image from "next/image";
 
 export default function MarkdownViewer({ content }: { content: string }) {
   return (
-    <ReactMarkdown className="prose lg:prose-xl" remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown
+      className="prose max-w-none"
+      components={{
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || "");
+          return !inline && match ? (
+            <SyntaxHighlighter
+              style={vscDarkPlus}
+              language={match[1]}
+              PreTag="div"
+              {...props}
+            >
+              {String(children).replace(/\n$/, "")}
+            </SyntaxHighlighter>
+          ) : (
+            <code className={className} {...props}>
+              {children}
+            </code>
+          );
+        },
+        // img: (image) => (
+        //   <Image
+        //     className="w-full max-h-60 object-cover"
+        //     src={image.src || ""}
+        //     alt={image.alt || ""}
+        //     width={500}
+        //     height={500}
+        //   />
+        // ),
+      }}
+    >
       {content}
     </ReactMarkdown>
   );
