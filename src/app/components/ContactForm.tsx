d5fef3bda2e20f76773f2e95982d930ba69047f2
junoshon <sanoldfox@naver.com>
2024-01-19 02:56:37 +0900

    feat: 입력 폼 구현

--- src/app/components/ContactForm.tsx
+++ src/app/components/ContactForm.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+'use client';
 
+import React, {ChangeEvent, FormEvent, useState} from 'react';
+
+type Form = {
+    from: string;
+    subject: string;
+    message: string;
+}
 export default function ContactForm() {
+    const [form, setForm] = useState<Form>({from: '', subject: '', message: ''});
+    const onChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+
+        setForm((prev) => ({...prev,[name]:value}));
+    }
+    const onSubmit = (e:FormEvent<HTMLFormElement>) =>{
+        e.preventDefault()
+        console.log(form)
+    }
     return (
-        <div>ContactForm</div>
+        <section>
+            <form onSubmit={onSubmit}>
+                <label htmlFor='from'>Your Email</label>
+                <input type="email" id='from' name='from' required autoFocus value={form.from} onChange={onChange}/>
+
+                <label htmlFor='subject'>Subject</label>
+                <input type="text" id='subject' name='subject' required  value={form.subject} onChange={onChange}/>
+
+                <label htmlFor='message'>Message</label>
+                <textarea id='message' name='message' required  value={form.message} rows={10} onChange={onChange}/>
+                <button>Submit</button>
+            </form>
+        </section>
     )
 }
